ac6b12ff858a4b3d98cb37836301a26cf0edb225
brokiem <milyhqvideo@gmail.com>
2023-04-15 15:02:45 +0800

    Add base files

---
+++ src/main/java/com/brokiem/bedrockreplay/BedrockReplay.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 brokiem
+ * This project is licensed under the MIT License
+ */
+
+package com.brokiem.bedrockreplay;
+
+public class BedrockReplay {
+
+    public static void main(String[] args) {
+        System.out.println("BedrockReplay");
+    }
+}
